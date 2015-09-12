ec07fee0726b255c3788e8bd2c5ec3d7ec39f84f
LandoB <jrolandobernal@gmail.com>
2015-09-12 13:46:37 -0500

    Working on my modals

--- rolando-bernal/js/discovery.js
+++ rolando-bernal/js/discovery.js
@@ -1,41 +1,41 @@
-/*------------------------------------------------------ 
-  WEBSITE LOADER            
+/*------------------------------------------------------
+	WEBSITE LOADER
 -------------------------------------------------------*/
 
 jQuery(window).load(function() {
-        // will first fade out the loading animation
+				// will first fade out the loading animation
 	jQuery(".status").fadeOut();
-        // will fade out the whole DIV that covers the website.
+				// will fade out the whole DIV that covers the website.
 	jQuery(".preloader").delay(1000).fadeOut("slow");
 });
 
-/*------------------------------------------------------ 
-  Smooth Scroll    
+/*------------------------------------------------------
+	Smooth Scroll
 -------------------------------------------------------*/
 
 $('.smooth').on('click',function (e) {
-	    e.preventDefault();
+			e.preventDefault();
 
-	    var target = this.hash,
-	    $target = $(target);
+			var target = this.hash,
+			$target = $(target);
 
-	    $('html, body').stop().animate({
-	        'scrollTop': $target.offset().top
-	    }, 800, 'swing', function () {
-	        window.location.hash = target;
-	    });
+			$('html, body').stop().animate({
+					'scrollTop': $target.offset().top
+			}, 800, 'swing', function () {
+					window.location.hash = target;
+			});
 });
 
  jQuery(document).ready(function($) {
 /*------------------------------------------------------
-  Navigation Features           
+	Navigation Features
 -------------------------------------------------------*/
 
 	if ($(window).scrollTop()===0){
 			$('#main-navigation').removeClass('stuck');
 		}
 		else{
-			$('#main-navigation').addClass('stuck');    
+			$('#main-navigation').addClass('stuck');
 		}
 
 		$(window).scroll(function(){
@@ -43,40 +43,40 @@ $('.smooth').on('click',function (e) {
 				$('#main-navigation').removeClass('stuck');
 			}
 			else{
-				$('#main-navigation').addClass('stuck');    
+				$('#main-navigation').addClass('stuck');
 			}
 		});
-		
+
 /*------------------------------------------------------
 	Adjust the #home height to be equal to the browsers height
 -------------------------------------------------------*/
 
 	$('#home').css({ 'height': $(window).height() });
-	   $(window).on('resize', function() {
+		 $(window).on('resize', function() {
 
 			$('#home').css({ 'height': $(window).height() });
 			$('body').css({ 'width': $(window).width() })
-	   });	
+		 });
 
 /*------------------------------------------------------
 	Resume Cards Click Events
 -------------------------------------------------------*/
 
 	/*Open*/
-    $(".expander").click(function(event) {
-        event.preventDefault();
-        $('.card-expand').css("transform", "translateY(-100%)");
+		$(".expander").click(function(event) {
+				event.preventDefault();
+				$('.card-expand').css("transform", "translateY(-100%)");
 		$('.card-expand').removeClass("card-hidden");
-        var card = $(this).attr("href");
-        $(".card-expand").not(card).css("transform", "translateY(0%)");
-    });
-	
+				var card = $(this).attr("href");
+				$(".card-expand").not(card).css("transform", "translateY(0%)");
+		});
+
 	/*Close*/
-    $(".card-expand i.fa").click(function(event) {
-        event.preventDefault();
-        var card = $(this).attr("href");
-        $(".card-expand").not(card).css("transform", "translateY(0%)");
-    });
+		$(".card-expand i.fa").click(function(event) {
+				event.preventDefault();
+				var card = $(this).attr("href");
+				$(".card-expand").not(card).css("transform", "translateY(0%)");
+		});
 
 /*------------------------------------------------------
 	Back To Top
@@ -90,7 +90,7 @@ $('.smooth').on('click',function (e) {
 			$('.scrollToTop').fadeOut();
 		}
 	});
-	
+
 	//Click event to scroll to top
 	$('.scrollToTop').click(function(){
 		$('html, body').animate({scrollTop : 0},800);
@@ -103,37 +103,37 @@ $('.smooth').on('click',function (e) {
 -------------------------------------------------------*/
 
 	var $lightbox = $('#lightbox');
-    
-    $('[data-target="#lightbox"]').on('click', function(event) {
-        var $img = $(this).find('img'), 
-            src = $img.attr('src'),
-            alt = $img.attr('alt'),
-            css = {
-                'maxWidth': $(window).width() - 100,
-                'maxHeight': $(window).height() - 100
-            };
-		
+
+		$('[data-target="#lightbox"]').on('click', function(event) {
+				var $img = $(this).find('img'),
+						src = $img.attr('src'),
+						alt = $img.attr('alt'),
+						css = {
+								'maxWidth': $(window).width() - 100,
+								'maxHeight': $(window).height() - 100
+						};
+
 		//Calls to pass the picture and title into the modal
 		var captext = $(this);
 		var caption = captext.data('id');
 		$(".modal-caption #caption").html(caption);
-		
-        $lightbox.find('.close').addClass('hidden');
-        $lightbox.find('img').attr('src', src);
-        $lightbox.find('img').attr('alt', alt);
-        $lightbox.find('img').css(css);
-    });
-    
-    $lightbox.on('shown.bs.modal', function (e) {
-        var $img = $lightbox.find('img');
-            
-        $lightbox.find('.modal-dialog').css({'width': $img.width()});
-        $lightbox.find('.close').removeClass('hidden');
-    });
+
+				$lightbox.find('.close').addClass('hidden');
+				$lightbox.find('img').attr('src', src);
+				$lightbox.find('img').attr('alt', alt);
+				$lightbox.find('img').css(css);
+		});
+
+		$lightbox.on('shown.bs.modal', function (e) {
+				var $img = $lightbox.find('img');
+
+				$lightbox.find('.modal-dialog').css({'width': $img.width()});
+				$lightbox.find('.close').removeClass('hidden');
+		});
 /*------------------------------------------------------
 	Initialize WOW Animations
 -------------------------------------------------------*/
 
 	new WOW().init();
-	
+
 });
