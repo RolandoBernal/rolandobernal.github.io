0a1be4f9d45c99b8da85b581df1a54b70825149c
landob <jrolandobernal@gmail.com>
2018-02-06 08:47:47 -0600

    Added animation to Nav-Bar

--- assets/js/nav-bar.js
+++ assets/js/nav-bar.js
@@ -3,19 +3,21 @@
     window.onscroll = function() {scrollFunction()};
     function scrollFunction() {
         if (document.body.scrollTop > 64 || document.documentElement.scrollTop > 64) {
-            document.getElementById("navbar").style.background = "rgba(10, 21, 37, 0.85)";
-            //document.getElementById("img-featured-navbar").style.display = "inline-block";
-            document.getElementById("img-featured-navbar").style.visibility = "visible";
-            document.getElementById("img-featured-navbar").style.opacity = "1";
-            document.getElementById("img-featured-navbar").style.transform = "scale(1,1)";
+            document.getElementById("navbar").style.visibility = "hidden";
+            document.getElementById("navbar-sm").style.visibility = "visible";
+            document.getElementById("navbar-sm").style.background = "rgba(10, 21, 37, 0.90)";
+            document.getElementById("img-featured-navbar-sm").style.visibility = "visible";
+            document.getElementById("img-featured-navbar-sm").style.opacity = "1";
+            document.getElementById("img-featured-navbar-sm").style.transform = "scale(1,1)";
             document.getElementById("img-featured-header").style.transform = "scale(0.5,0.5)";
             document.getElementById("img-featured-header").style.visibility = "hidden";
         } else {
+            document.getElementById("navbar").style.visibility = "visible";
             document.getElementById("navbar").style.background = "transparent";
-            //document.getElementById("img-featured-navbar").style.display = "none";
-            document.getElementById("img-featured-navbar").style.transform = "scale(0.5,0.5)";
-            document.getElementById("img-featured-navbar").style.visibility = "hidden";
-            document.getElementById("img-featured-navbar").style.opacity = "0";
+            document.getElementById("navbar-sm").style.visibility = "hidden";
+            document.getElementById("img-featured-navbar-sm").style.transform = "scale(0.5,0.5)";
+            document.getElementById("img-featured-navbar-sm").style.visibility = "hidden";
+            document.getElementById("img-featured-navbar-sm").style.opacity = "0";
             //document.getElementById("img-featured-header").style.display = "initial";
             document.getElementById("img-featured-header").style.visibility = "visible";
             document.getElementById("img-featured-header").style.opacity = "1";
