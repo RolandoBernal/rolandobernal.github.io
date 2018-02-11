148e922338efbf3ec8274510e6ac9e07ad932c54
landob <jrolandobernal@gmail.com>
2018-02-11 01:15:43 -0600

    Removed unnecessary files

--- assets/js/typewriter.js
+++ assets/js/typewriter.js
@@ -1,6 +1,7 @@
 // set up text to print, each item in array is new line
 var aText = new Array(
-"UI & UX Digital Product Designer, and Web Developer based in Music City USA "
+"Rolando Bernal",
+"UI & UX Digital Product Designer, and Web Developer based in Music City USA"
 );
 var iSpeed = 100; // time delay of print out
 var iIndex = 0; // start printing array at this posision
@@ -20,7 +21,7 @@ function typewriter()
  while ( iRow < iIndex ) {
   sContents += aText[iRow++] + '<br />';
  }
- destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + " <span id='typed-cursor' class='blinking'>|</span>";
+ destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + " <span id='typed-cursor' class='blinking'>_</span>";
  if ( iTextPos++ == iArrLength ) {
   iTextPos = 0;
   iIndex++;
@@ -34,7 +35,81 @@ function typewriter()
 }
 
 function myFunction() {
-    setTimeout(function(){ typewriter(); }, 3500);
+    setTimeout(function(){ typewriter(); }, 100);
 }
 
-myFunction();
+//myFunction();
+
+var t = document.getElementById("typewriter");
+//var HTML = t.innerHTML;
+
+function setupTypewriter() {
+        var HTML = t.innerHTML;
+
+        t.innerHTML = "";
+
+        var cursorPosition = 0,
+            tag = "",
+            writingTag = false,
+            tagOpen = false,
+            typeSpeed = 100,
+            tempTypeSpeed = 0;
+
+        var type = function() {
+        
+            if (writingTag === true) {
+                tag += HTML[cursorPosition];
+            }
+
+            if (HTML[cursorPosition] === "<") {
+                tempTypeSpeed = 0;
+                if (tagOpen) {
+                    tagOpen = false;
+                    writingTag = true;
+                } else {
+                    tag = "";
+                    tagOpen = true;
+                    writingTag = true;
+                    tag += HTML[cursorPosition];
+                }
+            }
+            if (!writingTag && tagOpen) {
+                tag.innerHTML += HTML[cursorPosition];
+            }
+            if (!writingTag && !tagOpen) {
+                if (HTML[cursorPosition] === " ") {
+                    tempTypeSpeed = 0;
+                }
+                else {
+                    tempTypeSpeed = (Math.random() * typeSpeed) + 50;
+                }
+                t.innerHTML += HTML[cursorPosition];
+            }
+            if (writingTag === true && HTML[cursorPosition] === ">") {
+                tempTypeSpeed = (Math.random() * typeSpeed) + 50;
+                writingTag = false;
+                if (tagOpen) {
+                    var newSpan = document.createElement("span");
+                    t.appendChild(newSpan);
+                    newSpan.innerHTML = tag;
+                    tag = newSpan.firstChild;
+                }
+            }
+
+            cursorPosition += 1;
+            if (cursorPosition < HTML.length - 1) {
+                setTimeout(type, tempTypeSpeed);
+            }
+
+        };
+
+        return {
+            type: type
+        };
+    }
+
+    var typer = document.getElementById('typewriter');
+
+    typewriter = setupTypewriter(typewriter);
+
+    typewriter.type();
