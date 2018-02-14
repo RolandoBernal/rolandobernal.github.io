447d9dfcc4bbef768d5fe15ec916e0b2671c846f
landob <jrolandobernal@gmail.com>
2018-02-14 14:49:10 -0600

    Refactoring NavBar

--- assets/js/nav-bar.js
+++ assets/js/nav-bar.js
@@ -2,9 +2,9 @@
 window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
     if (document.body.scrollTop > 64 || document.documentElement.scrollTop > 64) {
-        document.getElementById("navbar").style.visibility = "hidden";
-        document.getElementById("navbar-sm").style.visibility = "visible";
-        document.getElementById("navbar-sm").style.background = "rgba(10, 21, 37, 0.90)";
+        //document.getElementById("myNavbar").style.visibility = "hidden";
+        //document.getElementById("navbar-sm").style.visibility = "visible";
+        document.getElementById("myNavbar").style.background = "rgba(10, 21, 37, 0.90)";
         document.getElementById("img-featured-navbar-sm").style.visibility = "visible";
         //document.getElementById("img-featured-navbar-sm").style.display = "inline-block";
         document.getElementById("img-featured-navbar-sm").style.opacity = "1";
@@ -12,9 +12,9 @@ function scrollFunction() {
         document.getElementById("img-featured-header").style.transform = "scale(0.5,0.5)";
         document.getElementById("img-featured-header").style.visibility = "hidden";
     } else {
-        document.getElementById("navbar").style.visibility = "visible";
-        document.getElementById("navbar").style.background = "transparent";
-        document.getElementById("navbar-sm").style.visibility = "hidden";
+        //document.getElementById("navbar").style.visibility = "visible";
+        document.getElementById("myNavbar").style.background = "transparent";
+        //document.getElementById("navbar-sm").style.visibility = "hidden";
         document.getElementById("img-featured-navbar-sm").style.transform = "scale(0.5,0.5)";
         document.getElementById("img-featured-navbar-sm").style.visibility = "hidden";
         //document.getElementById("img-featured-navbar-sm").style.display = "none";
@@ -26,18 +26,19 @@ function scrollFunction() {
 }
 //To display the different links of the menu: Tools, Projects, Expeience, About:
 function toggleFunction() {
-    var x = document.getElementById("navbar-mobile");
-    if (x.className === "navbar-mob") {
-        x.className += " responsive";
-    } else {
-        x.className = "navbar-mob";
-    }
-    var y = document.getElementById("nav-links");
-    if (y.className === "navbar-links") {
-        y.className += " responsive";
-    } else {
-        y.className = "navbar-links";
-    }
+//    var x = document.getElementById("navbar-mobile");
+//    if (x.className === "navbar-mob") {
+//        x.className += " responsive";
+//    } else {
+//        x.className = "navbar-mob";
+//    }
+//    var y = document.getElementById("nav-links");
+//    if (y.className === "navbar-links") {
+//        y.className += " responsive";
+//    } else {
+//        y.className = "navbar-links";
+//    }
+//    // Hamburguer Menu Icon
     var z = document.getElementById("nav-icon3");
     if (z.className === "menu-icon") {
         z.className += " open";
