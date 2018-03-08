93628e7fda93de5424813d9d96663b78ab727131
rolandobernal <jrolandobernal@gmail.com>
2018-03-08 10:30:32 -0600

    Fixed issue with tab buttons
    The buttons are now allowing to close a tab when a user clicks the
    buttons after the tab has been opened.

--- projects/BootstrapV4_Portfolio/assets/js/tabs.js
+++ projects/BootstrapV4_Portfolio/assets/js/tabs.js
@@ -7,25 +7,24 @@ function openSection(evt, sectionName) {
   tablinks = document.getElementsByClassName("tablinks");
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
-    //tablinks[i].className = tablinks[i].classList.add("no-active");
-  }
-  if (evt.currentTarget.classList.contains("no-active")) {
-    document.getElementById(sectionName).style.display = "block";
-    console.log("evt.currentTarget.className: (1) ", evt.currentTarget.className);
-    evt.currentTarget.classList.remove("no-active");
-    evt.currentTarget.className += " active showing";
-    console.log("evt.currentTarget.className: (2)", evt.currentTarget.className);
+    tablinks[i].innerHTML = "Learn more";
+    //console.log("--- assigned Learn More to the innerHTML ---");
+    if (evt.currentTarget.classList.contains("no-active")) {
+      document.getElementById(sectionName).style.display = "block";
+      console.log("evt.currentTarget.className: (1) ", evt.currentTarget.className);
+      evt.currentTarget.classList.remove("no-active");
+      evt.currentTarget.className += " active showing";
+      console.log("evt.currentTarget.className: (2)", evt.currentTarget.className);
+    }
   }
   if (evt.currentTarget.classList.contains("showing")) {
     evt.currentTarget.className = "tablinks active hiding";
-    console.log("evt.currentTarget.className: (3) ", evt.currentTarget.className);
-    evt.currentTarget.innerHTML = "Close";
-    console.log("evt.innerHTML -> ", evt.currentTarget.innerHTML);
+    //console.log("evt.currentTarget.className: (3) ", evt.currentTarget.className);
+    evt.currentTarget.innerHTML = "&times Close";
+    //console.log("evt.innerHTML -> ", evt.currentTarget.innerHTML);
   } else if (evt.currentTarget.classList.contains("hiding")) {
     evt.currentTarget.innerHTML = "Learn more";
     evt.currentTarget.className = "tablinks no-active";
-    console.log("evt.innerHTML: ", evt.currentTarget.innerHTML);
+    //console.log("evt.innerHTML: ", evt.currentTarget.innerHTML);
   }
 }
-
-// <span onclick="this.parentElement.style.display='none'" class="topright">&times</span>
