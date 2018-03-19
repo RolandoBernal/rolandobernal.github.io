4f8e2712f33325ccd21b1b859af2631c5e88a919
rolandobernal <jrolandobernal@gmail.com>
2018-03-19 09:55:40 -0500

    Updated footer links

--- projects/BootstrapV4_Portfolio/assets/js/tabs.js
+++ projects/BootstrapV4_Portfolio/assets/js/tabs.js
@@ -8,23 +8,23 @@ function openSection(evt, sectionName) {
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
     tablinks[i].innerHTML = "Learn more";
-    //console.log("--- assigned Learn More to the innerHTML ---");
+    console.log("--- assigned Learn More to the innerHTML ---");
     if (evt.currentTarget.classList.contains("no-active")) {
       document.getElementById(sectionName).style.display = "block";
-      //console.log("evt.currentTarget.className: (1) ", evt.currentTarget.className);
+      console.log("evt.currentTarget.className: (1) ", evt.currentTarget.className);
       evt.currentTarget.classList.remove("no-active");
       evt.currentTarget.className += " active showing";
-      //console.log("evt.currentTarget.className: (2)", evt.currentTarget.className);
+      console.log("evt.currentTarget.className: (2) ", evt.currentTarget.className);
     }
   }
   if (evt.currentTarget.classList.contains("showing")) {
     evt.currentTarget.className = "tablinks active hiding";
-    //console.log("evt.currentTarget.className: (3) ", evt.currentTarget.className);
+    console.log("evt.currentTarget.className: (3) ", evt.currentTarget.className);
     evt.currentTarget.innerHTML = "&times Close";
-    //console.log("evt.innerHTML -> ", evt.currentTarget.innerHTML);
+    console.log("evt.innerHTML -> ", evt.currentTarget.innerHTML);
   } else if (evt.currentTarget.classList.contains("hiding")) {
     evt.currentTarget.innerHTML = "Learn more";
     evt.currentTarget.className = "tablinks no-active";
-    //console.log("evt.innerHTML: ", evt.currentTarget.innerHTML);
+    console.log("evt.innerHTML: ", evt.currentTarget.innerHTML);
   }
 }
