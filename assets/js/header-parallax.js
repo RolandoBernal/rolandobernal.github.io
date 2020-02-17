02e10301686db7ecff262ff8338b325abf67d538
rolandobernal <jrolandobernal@gmail.com>
2020-02-16 19:01:41 -0600

    Updated to new favicon

--- assets/js/header-parallax.js
+++ assets/js/header-parallax.js
@@ -16,3 +16,97 @@ function scrollHandler() {
         }
     }
 }
+
+
+// <div class="lb_race_name">${datarecord.Name}</div>
+//         <div class="lb_race_rank ${(datarecord.NumberOfRoutines > 0) ? "checkmark" : "cross"}">
+//         ${(datarecord.NumberOfRoutines > 0) ? "✔" : "✗"}</div>
+//         <div class="lb_race_points"><span class="${
+//             datarecord.CheckMarkClass
+//         }">${
+//         datarecord.NumberOfRoutines
+
+
+
+        {/* const data = [
+    {
+        Name: "Marychuy",
+        NumberOfRoutines: 2,
+        Genre: "female",
+        ImageName: "ma",
+        // CheckMark: "✔",
+        // CheckMarkClass: "checkmark"
+    },
+    {
+        Name: "Bety",
+        NumberOfRoutines: 4,
+        Genre: "female",
+        ImageName: "cy"
+        // CheckMark: "✔",
+        // CheckMarkClass: "checkmark"
+    },
+    {
+        Name: "Cynthia",
+        NumberOfRoutines: 3,
+        Genre: "female",
+        ImageName: "cy"
+        // CheckMark: "✔",
+        // CheckMarkClass: "checkmark"
+    },
+    {
+        Name: "El Profe",
+        NumberOfRoutines: 5,
+        Genre: "male",
+        ImageName: "ep"
+        // CheckMark: "✔",
+        // CheckMarkClass: "checkmark"
+    },
+    {
+        Name: "Martha",
+        NumberOfRoutines: 4,
+        Genre: "female",
+        ImageName: "ma"
+        // CheckMark: "✔",
+        // CheckMarkClass: "checkmark"
+    },
+    // {
+    //     Name: "LiS",
+    //     NumberOfRoutines: 0,
+    //     Genre: "female",
+    //     ImageName: "0f",
+    //     CheckMark: "✗",
+    //     CheckMarkClass: "cross"
+    // },
+    {
+        Name: "Kary",
+        NumberOfRoutines: 2,
+        Genre: "female",
+        ImageName: "ka"
+        // CheckMark: "✔",
+        // CheckMarkClass: "checkmark"
+    },
+    {
+        Name: "Cristian",
+        NumberOfRoutines: 0,
+        Genre: "male",
+        ImageName: "0m"
+        // CheckMark: "✔",
+        // CheckMarkClass: "cross"
+    },
+    {
+        Name: "Emily",
+        NumberOfRoutines: 0,
+        Genre: "female",
+        ImageName: "0f"
+        // CheckMark: "✗",
+        // CheckMarkClass: "cross"
+    },
+    {
+        Name: "Coach",
+        NumberOfRoutines: 6,
+        Genre: "male",
+        ImageName: "to"
+        // CheckMark: "✔",
+        // CheckMarkClass: "checkmark"
+    }
+]; */}
